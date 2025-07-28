2d850887bc045a4bcad45852ac29803cf2d5bfe0
Sandy Machado <seuemail@exemplo.com>
2025-07-28 14:18:04 -0300

    Iniciando projeto

---
+++ src/main/java/com/ampara_pet/ampara_pet/AmparaPetApplication.java
@@ -0,0 +1,13 @@
+package com.ampara_pet.ampara_pet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmparaPetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmparaPetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ampara_pet/ampara_pet/AmparaPetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ampara_pet.ampara_pet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmparaPetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
